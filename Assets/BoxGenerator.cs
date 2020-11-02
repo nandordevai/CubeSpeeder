027cd7912e121ca911a86fa2724d579e8ee36446
Nandor Devai <mail@nandordevai.net>
2020-11-02 17:17:44 +0100

    set materials, update positions

--- Assets/BoxGenerator.cs
+++ Assets/BoxGenerator.cs
@@ -7,12 +7,14 @@ public class BoxGenerator : MonoBehaviour
     void Start()
     {
         GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
-        plane.transform.localScale = new Vector3(3, 1, 7);
+        plane.transform.localScale = new Vector3(4, 1, 7);
         plane.transform.position = new Vector3(0, 0, 25);
-        plane.GetComponent<Renderer>().material.color = Color.HSVToRGB(.6f, 0.9f, 1f);
+        MeshRenderer mr = plane.GetComponent<MeshRenderer>();
+        mr.material = new Material(Shader.Find("Diffuse"));
+        mr.material.color = Color.HSVToRGB(.6f, 0.9f, 1f);
         float z = 0;
         while (z < 50) {
-            float x = -12;
+            float x = -18;
             while (x < 10)
             {
                 x += (Random.value + 1) * 3;
@@ -35,10 +37,12 @@ public class BoxGenerator : MonoBehaviour
     private void CreateBoxAt(float x, float y, float z)
     {
         float height = RandomHeight();
-        GameObject BoxObj = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        BoxObj.transform.localScale = new Vector3(1, height, 1);
-        BoxObj.transform.position = new Vector3(x, y + height / 2, z);
-        BoxObj.GetComponent<Renderer>().material.color = new Color(255, 255, 255);
+        GameObject box = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        box.transform.localScale = new Vector3(1, height, 1);
+        box.transform.position = new Vector3(x, y + height / 2, z);
+        box.GetComponent<Renderer>().material.color = new Color(255, 255, 255);
+        MeshRenderer mr = box.GetComponent<MeshRenderer>();
+        mr.material = new Material(Shader.Find("Diffuse"));
     }
 
     void Update()
